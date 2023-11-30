dd8e876eaad75ab74965b757c1a9f47598038240
LidiaReb <81241178+LidiaReb@users.noreply.github.com>
2023-12-01 00:25:30 +0300

    Add files via upload
    Поправила класс кнопок

--- button.py
+++ button.py
@@ -1,38 +1,40 @@
 import pygame
 
 pygame.init()
-class button:
+
+class Button:
     '''
     x, y - int-координаты кнопки
     width, height - int - размеры кнопки
     color - цвет кнопки
     text - string - надпись на кнопке
+    size_text - размер надписи на кнопке
     '''
-    def __init__(self, x, y, width, height, color, text):
+    def __init__(self, x=10, y=10, width=100, height=10, color='grey', text='Ведите текст параметром text', size_text=36):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.color = color
         self.text = text
+        self.size_text = size_text
         self.pressed = False
-    def draw(self,button_pressed):
-        '''
-        функция рисует кнопку,красная - если кнопка нажата, свой цвет - если не нажата
-        '''
-        if button_pressed:
-            pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height))
-        else:
-            pygame.draw.rect(window, 'RED', (self.x, self.y, self.width, self.height))
 
-        font = pygame.font.Font(None, 36)
+    def draw(self, window: pygame.Surface):
+        '''
+        функция рисует кнопку, красная - если кнопка нажата, свой цвет - если не нажата
+        '''
+        if self.pressed:
+            pygame.draw.rect(window, 'RED', (self.x, self.y, self.width, self.height))
+        else:
+            pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height))
+
+        font = pygame.font.Font(None, self.size_text)
         text = font.render(self.text, True, 'BLACK')
         text_rect = text.get_rect(center=(self.x + self.width // 2, self.y + self.height // 2))
         window.blit(text, text_rect)
 
-
-
-    def get_pressed(self):
+    def get_pressed(self, event):
         '''
         возвращает True, если кнопка была нажата, False - если не нажата
         '''
@@ -42,5 +44,3 @@ class button:
                 self.pressed = True
         elif event.type == pygame.MOUSEBUTTONUP:
             self.pressed = False
-        return self.pressed
-
