8be8dc633c9148cf348c5a2223f06a3983385dab
LidiaReb <81241178+LidiaReb@users.noreply.github.com>
2023-12-03 02:55:55 +0300

    Add files via upload
    Добавлены объекты ресурсов, т.е. воды и электричества

--- objects.py
+++ objects.py
@@ -1,6 +1,8 @@
 import pygame
 from visual import*
 
+BLUE = (0, 119, 255)
+
 class Buildings(): 
     '''
     x, y - координаты дома
@@ -21,26 +23,46 @@ class Buildings():
 
     def draw(self):
         # FIXME-Лера подгрузка картинок домов и их отрисовка
+        r = 20
         if self.type == 1:
-            color = 'red'
-        if self.type == 2:
             color = 'orange'
+        if self.type == 2:
+            color = 'red'
         pygame.draw.circle(
             self.screen,
             color,
             (int(self.x), int(self.y)),
-            50
+            r
+        )
+
+class Resources():
+    def __init__(self, x, y, type, screen):
+        self.screen = screen
+        self.type = type
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        if self.type == 1:
+            color = BLUE
+        if self.type == 2:
+            color = 'yellow'
+        pygame.draw.circle(
+            self.screen,
+            color,
+            (int(self.x), int(self.y)),
+            30
         )
 
 class Roads():
     def __init__(self, x, y, type, level):
         self.type = type
-        self.level = level
-        self.x = x
-        self.y = y
+        self.begining_x = x
+        self.begining_y = y
+        self.end_x = x
+        self.end_y = y
         '''
-        type - тип дороги
-        level - уровень 
+        type - тип дороги: 1 - обычная, 2 - водная, 3 - электро
         x,y - координаты ВЕРХНЕГО ЛЕВОГО угла квадрата, где располагается с
         '''
 
