4774dfa6f6e8c7f00ac0d193e73a7ec4deaa5e67
Popugairulat <150455922+Popugairulat@users.noreply.github.com>
2023-12-05 14:04:57 +0300

    теперь объекты можно строить на соседних клетках

--- field_data_functions.py
+++ field_data_functions.py
@@ -1,14 +1,14 @@
 WIDTH = 1200
 HEIGHT = 750
-len_width = int(WIDTH/(12*4))
-len_height = int(HEIGHT/(7.5*4))
+len_width = int(WIDTH/(16))
+len_height = int(HEIGHT/(10))
 
-HOUSE_LEN = len_width*6
-HOUSE_HIGHT = len_height*3
-WATER_LEN = len_width*4
-WATER_HIGHT = len_height*3
-ELECTRICITY_LEN = len_width*3
-ELECTRICITY_HIGHT = len_height*3
+HOUSE_LEN = len_width*2
+HOUSE_HIGHT = len_height*1
+WATER_LEN = len_width*1
+WATER_HIGHT = len_height*1
+ELECTRICITY_LEN = len_width*1
+ELECTRICITY_HIGHT = len_height*1
 def add_data(what_you_build, building_data, obj):
     '''
     функция записывает в массив данные об узлах сетки, занятой постройкой
@@ -17,16 +17,16 @@ def add_data(what_you_build, building_data, obj):
     0 - узел свободен, 1 - узел занят
     '''
     if what_you_build == 'house':
-        for i in range(HOUSE_HIGHT//len_height+1):
-            for j in range(HOUSE_LEN//len_width+1):
+        for i in range(HOUSE_HIGHT//len_height):
+            for j in range(HOUSE_LEN//len_width):
                 building_data[obj.y//len_height + i][obj.x//len_width + j] = 1
     if what_you_build == 'electricity':
-        for i in range(ELECTRICITY_HIGHT//len_height+1):
-            for j in range(ELECTRICITY_LEN//len_width + 1):
+        for i in range(ELECTRICITY_HIGHT//len_height):
+            for j in range(ELECTRICITY_LEN//len_width ):
                 building_data[obj.y//len_height + i][obj.x//len_width + j] = 1
     if what_you_build == 'water':
-        for i in range(WATER_HIGHT//len_height+1):
-            for j in range(WATER_LEN//len_width + 1):
+        for i in range(WATER_HIGHT//len_height):
+            for j in range(WATER_LEN//len_width):
                 building_data[obj.y//len_height + i][obj.x//len_width + j] = 1
     
     return building_data
@@ -38,27 +38,27 @@ def check_the_place(what_you_build, building_data, x, y):
     '''
     flag = True
     if what_you_build == 'house':
-        if y//len_height + HOUSE_HIGHT//len_height + 1 > HEIGHT//len_height or x//len_width + HOUSE_LEN//len_width + 1> WIDTH//len_width:
+        if y//len_height + HOUSE_HIGHT//len_height +1> HEIGHT//len_height or x//len_width + HOUSE_LEN//len_width +1> WIDTH//len_width:
             flag = False
         else:
-            for i in range(3):
-                for j in range(4):
+            for i in range(1):
+                for j in range(2):
                     if building_data[y//len_height + i][x//len_width + j] == 1:
                         flag = False
     elif what_you_build == 'electricity':
         if y//len_height + ELECTRICITY_HIGHT//len_height+1 > HEIGHT//len_height or x//len_width + ELECTRICITY_LEN//len_width + 1> WIDTH//len_width:
             flag = False
         else:
-            for i in range(ELECTRICITY_HIGHT//len_height+1):
-                for j in range(ELECTRICITY_LEN//len_width + 1):
+            for i in range(ELECTRICITY_HIGHT//len_height):
+                for j in range(ELECTRICITY_LEN//len_width ):
                     if building_data[y//len_height + i][x//len_width + j] == 1:
                         flag = False
     elif what_you_build == 'water':
         if y//len_height + WATER_HIGHT//len_height+1 > HEIGHT//len_height or x//len_width + WATER_LEN//len_width + 1> WIDTH//len_width:
             flag = False
         else:
-            for i in range(WATER_HIGHT//len_height+1):
-                for j in range(WATER_LEN//len_width + 1):
+            for i in range(WATER_HIGHT//len_height):
+                for j in range(WATER_LEN//len_width):
                     if building_data[y//len_height + i][x//len_width + j] == 1:
                         flag = False
     return flag
